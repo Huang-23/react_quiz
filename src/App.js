179c6eda0cdae58ff35e0bc582333220a9e4d3e9
黃紹禎 <joanna@huangshaozhendeMacBook-Pro.local>
2021-04-28 16:40:57 +0800

    4

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import HelloCGU from './cgu_hello'
-
-const changeText=(event)=>{
-  console.log(event.target)
-  event.target.innerText = event.target.innerText + "被點了"
-}
-const MultiButton=(num)=>{
-  var output=[];
-  for(let i=1;i<num+1;++i)
-      output.push(<button onClick={changeText}>第{i}個按鍵</button>)
-  return output;
-}
+import MultiButton from './cgu_multiButton'
 
 function App() {
   return (
